b904da9cf518a37ddeca1d3089cf84be684ac3eb
moodylife <36269891+moodylife@users.noreply.github.com>
2018-12-28 16:23:35 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,65 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setGame('هخه شخة شقلح بقلح','https://www.twitch.tv/peery13');
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+client.on('ready', async() => {
+var server = "512245795026894850"; 
+var channel = "528183470070169602";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز ')
+    },305);
+})
+
+client.on('ready', async() => {
+var server = "512245795026894850"; 
+var channel = "528183470070169602";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+var prefix = "*";
+const pr1 = ['525397055812665346' , '502907623641972757' , ''];//{ID Who Can Use Commands}
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+   
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+   
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.channel.send('${prefix}say <words>'); 
+   
+  if (command == "say") {
+            if (!pr1.includes(message.author.id)) return;
+   
+  message.channel.send(args.join("  "))
+      message.delete();
+    }
+  });
+client.login(process.env.BOT_TOKEN);
